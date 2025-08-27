b3c824d8a686f062533b550e620989b245c81b34
KevinLarios143-01 <alexander.larios@galileo.edu>
2025-08-27 08:59:49 -0600

    Se versionan cambios en el sistema

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  apiUrl: process.env['API_URL'] || 'https://tu-api.railway.app/api',
   firebase: {
     apiKey: "************************************",
     authDomain: "**********************************************",
