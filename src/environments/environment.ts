2e786da9425479d7943718e0ea993fd8a8721334
KevinLarios143-01 <alexander.larios@galileo.edu>
2025-08-27 09:16:44 -0600

    Se suben cambios al sistema

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   production: false,
 
 
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://tu-api.railway.app/api',
 
   firebase: {
     apiKey: "***************************************",
