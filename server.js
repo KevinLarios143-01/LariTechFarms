87c3dd6bfb0eab5de7247de708e05a684adcb1ad
KevinLarios143-01 <alexander.larios@galileo.edu>
2025-08-27 09:18:24 -0600

    Pruebas

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/preview'));
+app.use(express.static(__dirname + '/dist/preview/browser'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/preview/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/preview/browser/index.html'));
 });
 
 const port = process.env.PORT || 8081;
