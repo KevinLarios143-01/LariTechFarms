63afc61b99e4421f4a6a1e4a0b00dda41a05bbf0
KevinLarios143-01 <alexander.larios@galileo.edu>
2025-09-19 16:38:19 -0600

    se versionan

--- dist/preview/browser/chunk-DFR7G4ST.js
+++
@@ -1,1 +0,0 @@
-var a={production:!1,apiUrl:"http://localhost:3001/api",firebase:{apiKey:"AIzaSyDZtT9KF1lngCvtsyIGW4T532ojKJD9BIo",authDomain:"fl-farms-gl.firebaseapp.com",projectId:"fl-farms-gl",storageBucket:"fl-farms-gl.firebasestorage.app",messagingSenderId:"210556388433",appId:"1:210556388433:web:be15faf02a843f70cf2470",measurementId:"*********************"}};export{a};
